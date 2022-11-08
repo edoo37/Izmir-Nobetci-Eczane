0cc9e4c2c7faaae6f6705901b1f5a68146271cdf
yasocoder <yasinsenel3437@gmail.com>
2022-11-09 02:15:00 +0300

    Splash Screen

---
+++ app/src/main/java/com/yasinsenel/izmireczaneuygulamasi/view/ViewPagerFragment.kt
@@ -0,0 +1,40 @@
+package com.yasinsenel.izmireczaneuygulamasi.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yasinsenel.izmireczaneuygulamasi.R
+import com.yasinsenel.izmireczaneuygulamasi.adapter.ViewPagerAdapter
+import com.yasinsenel.izmireczaneuygulamasi.databinding.FragmentViewPagerBinding
+import com.yasinsenel.izmireczaneuygulamasi.fragment.FirstFragment
+import com.yasinsenel.izmireczaneuygulamasi.fragment.SecondFragment
+import com.yasinsenel.izmireczaneuygulamasi.fragment.ThirdFragment
+import kotlinx.android.synthetic.main.fragment_view_pager.*
+
+
+class ViewPagerFragment : Fragment() {
+    private lateinit var binding : FragmentViewPagerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentViewPagerBinding.inflate(inflater,container,false)
+
+        val fragmentList : ArrayList<Fragment> = arrayListOf()
+        fragmentList.add(FirstFragment())
+        fragmentList.add(SecondFragment())
+        fragmentList.add(ThirdFragment())
+
+        val adapter = ViewPagerAdapter(fragmentList,requireActivity().supportFragmentManager,lifecycle)
+        viewPager2.adapter = adapter
+
+        return binding.root
+    }
+}
