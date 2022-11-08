dbb84bf98b7b74ce4a3e5d2d31d70c47c91a9e48
yasocoder <yasinsenel3437@gmail.com>
2022-11-09 02:22:52 +0300

    Splash Fragment

--- app/src/main/java/com/yasinsenel/izmireczaneuygulamasi/fragment/SplashFragment.kt
+++ app/src/main/java/com/yasinsenel/izmireczaneuygulamasi/fragment/SplashFragment.kt
@@ -1,15 +1,19 @@
 package com.yasinsenel.izmireczaneuygulamasi.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.yasinsenel.izmireczaneuygulamasi.R
+import com.yasinsenel.izmireczaneuygulamasi.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-
+    private lateinit var binding:FragmentSplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,7 +22,11 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+        },3000)
+        binding = FragmentSplashBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
     }
 }
